9e99b1221ca4d43fdcaec4c0b1c99cbfecb4d366
kameikay <kameikay@gmail.com>
2020-09-15 19:01:32 -0300

    versao 1.0.8.4

--- noticias.py
+++ noticias.py
@@ -31,15 +31,14 @@ while True:
             resultados.append({'PAR': moeda, 'HORÁRIO': horario, 'IMPACTO': impacto, 'HORARIO2': horario2, 'NOTÍCIA': noticia})
 
     while True:
-        #horario_agora = float(str(datetime.datetime.now())[11:16].replace(':', '.'))
-        horario_agora = 2.0
+        horario_agora = float(str(datetime.datetime.now())[11:16].replace(':', '.'))
         if horario_agora <= 23.59 or horario_agora >= 00.01: 
             for info in resultados:
                 if round(float(info['HORARIO2'] - 1), 2) == horario_agora:
                     print(f'''{exclamacao}ATENÇÃO, ÁGUIAS! NOTÍCIA {exclamacao}\nPARIDADE: {info["PAR"]}\nHORÁRIO: {info["HORÁRIO"]}\nNOTÍCIA: {info["NOTÍCIA"]}\nIMPACTO: {info["IMPACTO"]}\n-----------------------------''')
                     bot.sendMessage(-481423284, f'''{exclamacao}ATENÇÃO, ÁGUIAS! NOTÍCIA {exclamacao}\nPARIDADE: {info["PAR"]}\nHORÁRIO: {info["HORÁRIO"]}\nNOTÍCIA: {info["NOTÍCIA"]}\nIMPACTO: {info["IMPACTO"]}\n-----------------------------''')
                 sleep(0.5)
-                #horario_agora = float(str(datetime.datetime.now())[11:16].replace(':', '.'))
+                horario_agora = float(str(datetime.datetime.now())[11:16].replace(':', '.'))
 
 
         else:
