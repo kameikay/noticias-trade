37d2f26934e1f12feb64f991032bf30c9902de36
kameikay <kameikay@gmail.com>
2020-09-17 00:03:37 -0300

    version 1.0.10

--- noticias.py
+++ noticias.py
@@ -41,7 +41,7 @@ while True:
             horario_agora = float(str(datetime.datetime.now())[11:16].replace(':', '.')) -3
             print(horario_agora)
         else:
-            sleep(1800)
+            sleep(300)
             break
 
 #versao 1.0.10 - 16/09/2020
